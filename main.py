8ea8f87d19e1c9b97b9a7f1c585a33da7811b73c
troy600 <bazartroy531@gmail.com>
2025-05-03 23:14:35 +0800

    usage example

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from Decisions.processing import Game
 
 def main():
     game = Game()
-    
     first = get_input("first card: ")
     second = get_input("second card: ")
     dealer = get_input("dealer faced card: ")
